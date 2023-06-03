332e06e7f84146c96c96299dc6f50e14da22ae7e
Alexander Degtarov <aleksandr.degtyarev@nexign.com>
2023-06-03 09:50:16 +0300

    connect client and server sockets

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,5 +1,20 @@
 import { createStore } from 'vuex'
 
+const {
+  VUE_APP_SERVER_ADDRESS,
+  VUE_APP_SERVER_WS,
+} = process.env
+
+const wsAddress = `${VUE_APP_SERVER_WS}://${VUE_APP_SERVER_ADDRESS}/echo`
+const ws = new WebSocket(wsAddress)
+
+ws.onopen = () => console.log('opened')
+ws.onmessage = data => console.log(data)
+ws.onclose = console.log
+ws.onerror = console.error
+
+document.onclick = e => ws.send("hello")
+
 export default createStore({
   state: {
   },
